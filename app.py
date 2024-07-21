39d6d59bcc7154ee231121ee855e21274e599007
Iain Jackson <iainjackson90@gmail.com>
2024-07-21 09:56:07 +0000

    Add: pager_powdery_mildew_detection

--- app.py
+++ app.py
@@ -4,11 +4,13 @@ from app_pages.multipage import MultiPage
 # load pages scripts
 from app_pages.page_summary import page_summary_body
 from app_pages.page_cherryleaves_visualizer import page_cherryleaves_visualizer_body
+from app_pages.page_powdery_mildew_detection import page_powdery_mildew_detector_body
 
 app = MultiPage(app_name="Powdery - Mildew Detection for chery leaves")  # Create an instance of the app
 
 # # Add your app pages here using .add_page()
 app.add_page("Project Summary", page_summary_body)
 app.add_page("Cherryleaves Visualizer", page_cherryleaves_visualizer_body)
+app.add_page("Powdery mildew detection", page_powdery_mildew_detector_body)
 
 app.run()  # Run the app
